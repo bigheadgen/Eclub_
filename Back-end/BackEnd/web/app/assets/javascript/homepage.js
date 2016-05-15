d47f03c7294eb7bf96f797c7bd1296c959d745f6
bigheadgen <gustavo_enndi@hotmail.com>
2016-05-15 20:56:43 -0300

    arquivo js da pagina home

--- Back-end/BackEnd/web/app/assets/javascript/homepage.js
+++ Back-end/BackEnd/web/app/assets/javascript/homepage.js
@@ -1,25 +1,28 @@
+var cidades = ["recife", "olinda", 
+"jaboatao", "caruaru", "petrolina","cabo", "camaragibe", 
+"garanhuns", "vitoria"];
+
 function myDivulgadores (){
 
 	window.location = 'page_login_divulgador.html';
 
 }
 function myPesquisar(){
-    var valor_busca;
-	var valor_busca = document.getElementById("busca").value;
 
-if(valor_busca == "recife" ||valor_busca == "jaboatao" || 
-	valor_busca == "olinda"|| valor_busca == "caruaru" || valor_busca == "petrolina" ||
-	 valor_busca == "cabo" || valor_busca == "camaragibe" || valor_busca == "garanhuns" || 
-	 valor_busca == "vitoria"){
+	var valor_busca = document.getElementById("busca").value.toLowerCase();
 
-window.location = 'page_principal_mapa.html';
-}
-else{
-if (valor_busca == "" || valor_busca == null){
-   	window.alert("Ops, digite uma cidade!");
-}
-else{
- window.alert("Ops, essa cidade não existe!");
-}
-}
+	for (var i = cidades.length - 1; i >= 0; i--) {
+		
+		if(valor_busca == cidades[i]){
+				
+			window.location = 'page_principal_mapa.html';
+		}
+	}
+	if(valor_busca == "" || valor_busca == null){
+
+   		window.alert("Ops, digite uma cidade!");	
+	}
+	else{
+	window.alert("Ops, essa cidade não existe!");
+	}
 }
