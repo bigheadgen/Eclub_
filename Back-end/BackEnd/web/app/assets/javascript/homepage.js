b2cbf693c7b7d0d0f31ae2577ecbf2973b891dac
Amanda <amandasilva.santos@outlook.com>
2016-05-15 17:54:46 -0300

    implemenando o javascript do campo de busca. Não está finalizando pq a lógica não traz nome de cidade maiscula.Apenas minuscula

--- Back-end/BackEnd/web/app/assets/javascript/homepage.js
+++ Back-end/BackEnd/web/app/assets/javascript/homepage.js
@@ -1,10 +1,25 @@
-function goDivulgadores (){
+function myDivulgadores (){
 
 	window.location = 'page_login_divulgador.html';
 
 }
-function validar(){
+function myPesquisar(){
+    var valor_busca;
+	var valor_busca = document.getElementById("busca").value;
 
-	var busca = document.getElementById("busca").value;
-	window.location = 'page_principal_mapa.html';
+if(valor_busca == "recife" ||valor_busca == "jaboatao" || 
+	valor_busca == "olinda"|| valor_busca == "caruaru" || valor_busca == "petrolina" ||
+	 valor_busca == "cabo" || valor_busca == "camaragibe" || valor_busca == "garanhuns" || 
+	 valor_busca == "vitoria"){
+
+window.location = 'page_principal_mapa.html';
+}
+else{
+if (valor_busca == "" || valor_busca == null){
+   	window.alert("Ops, digite uma cidade!");
+}
+else{
+ window.alert("Ops, essa cidade não existe!");
+}
+}
 }
