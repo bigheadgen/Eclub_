1b849cabb8b1e546e756bd7daa1de4b4ea22120f
Leandro Moreira <leandro10_zoy@hotmail.com>
2016-05-17 20:28:36 -0300

    Organização dos DAO's da classe EVento

--- Back-end/BackEnd/src/java/br/com/eclub/dao/EventoDAO.java
+++ Back-end/BackEnd/src/java/br/com/eclub/dao/EventoDAO.java
@@ -1,4 +1,5 @@
 package br.com.eclub.dao;
+
 import br.com.eclub.modelo.Evento;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -6,41 +7,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  *
  * @author Gustavo, Thierry Freire
  */
 public class EventoDAO {
-    private Connection con;
 
-    //metodo que vai trazer todos os eventos de um local
-    public ArrayList<Evento> getEventos(String localBusca) {
-       
-        
-        //adicionar o retorno correto 
-        return null;
-    }   
-    public int buscarEvento(Evento evento) throws SQLException{
-        int id = -1;
-        List <Evento> eventos = new ArrayList ();
-        //query que busca o evento no banco
-       con = new ConnectionFactory().getConnection();
-       PreparedStatement stmt = con.prepareStatement("SELECT FROM Evento WHERE nome = ?"); 
-       stmt.setString(1, evento.getNomeEvento());
-       ResultSet rs = stmt.executeQuery();
-       while (rs.next()){
-           //implementar while para término do método
-       }
-       con.close();
-       return id;
+    public Connection conLocal;
+
+    public EventoDAO() {
+        ConnectionFactory con = new ConnectionFactory();
+        conLocal = con.getConnection();
     }
-        
-    public void removerEvento(Evento evento) throws SQLException{
-    
-       int id = buscarEvento(evento);
-    //query que deleta o evento do banco
-       
-    
+
+    public void inserirEvento(Evento evento) {
+        Evento e = new Evento();
+        e = evento;
+        String sql = "";
+
     }
-    
 }
