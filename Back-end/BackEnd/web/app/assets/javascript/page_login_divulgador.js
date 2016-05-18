2639531d773a49a8d64c7ef1f06e9b1839aa9483
GugaE <gustavo_enndi@hotmail.com>
2016-05-18 18:33:49 -0300

    javascript no campo de busca na pagina do mapa

--- Back-end/BackEnd/web/app/assets/javascript/page_login_divulgador.js
+++ Back-end/BackEnd/web/app/assets/javascript/page_login_divulgador.js
@@ -1,4 +1,5 @@
-
+var emailsValidos = ["hotmal.com" , "outlook.com", "gmail.com"];
+	
 	function esqueciMinhaSenha(){
 
 		var email = prompt("Enviaremos uma nova senha para seu email");
@@ -7,4 +8,27 @@
 	function cadastraDivulgador(){
 
 		var email = prompt("Informe seu email");	
+	}
+
+	function validarEmail(){
+
+		var email = document.getElementById("box").value.toLowerCase();
+		var letra = email.indexOf("@");
+
+		for (var i = 0; i < emailsValidos.length; i++) {
+		 
+			if(email == emailsValidos[i]){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function logarDivulgador(){
+		
+		if(validarEmail){
+
+			alert("email valido");
+		}
+
 	}
