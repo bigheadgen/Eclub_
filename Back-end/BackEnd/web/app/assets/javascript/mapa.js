e13c1d3690646bdc31b822763edadd83252cfcdb
bigheadgen <gustavo_enndi@hotmail.com>
2016-05-19 04:58:54 -0300

    finalizado o javascript do mapa podendo colocar varios pontos e tb o zoom automatico para mostrar todos os pontos que foram adicionados tb foi colocado o balao de informações do evento no ponto que ao clickar no ponto o balao aparece porem falta personalizar o balao para ficar de acordo com o projeto

--- Back-end/BackEnd/web/app/assets/javascript/mapa.js
+++ Back-end/BackEnd/web/app/assets/javascript/mapa.js
@@ -3,13 +3,23 @@ var pino1 = {
 	lat: -8.063167002249708, 
 	long:-34.871083450000015
 };
-
 var pino2 = { 
 	name: "FG", 
 	lat: -8.162551662756185,
 	long:-34.91668672513958
 };
+var pino3 = { 
+  name: "Paiva", 
+  lat: -8.26005792,
+  long:-34.94514942
+};
+var pino4 = { 
+  name: "Ponta Negra ", 
+  lat: -5.89562116,
+  long:-35.15552044
+};
 
+var locais1 = [pino1, pino2, pino3, pino4];
 //FUNÇÃO QUE FAZ O MARCADOR CAIR NO MAPA
 function toggleBounce() {
  	
@@ -20,30 +30,54 @@ function toggleBounce() {
     	marker.setAnimation(google.maps.Animation.BOUNCE);
   	}
 }
-// AQUI VAI SER A FUNÇÃO QUE CRIA E ADICIONA AO MAPA O MARCARDOR
-function addMarker(){
+// AQUI A CAIXA DE INFORMAÇÕES DO PONTO E CRIADA
+function contentString(map , marker){
+    var contentString = '<div id="boxIW">'+
 
+    '</div>';
+    
+    var infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    size: new google.maps.Size(50, 50)
+    });
+      
+      marker.addListener('click', function(){
+      infowindow.open(map , marker);
+     });
+
+}
+// AQUI VAI SER A FUNÇÃO QUE CRIA E ADICIONA AO MAPA O MARCARDOR
+// FUNÇÃO QUE ADICONA N LOCAIS AO MAPA
+function addMarker(map, locais){
+    
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < locais.length; i++) {  
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locais[i].lat, locais[i].long),
+      map: map,
+      title: locais[i].name 
+    });
+      
+      contentString(map , marker);
+      bounds.extend(new google.maps.LatLng(locais[i].lat, locais[i].long));
+  }
+
+     map.fitBounds(bounds);
 }
 function initMap() {
   		var map;
   		//VARIAVEL QUE TEM O LOCAL DO MARCADOR
-		var myLatLng = {lat: pino1.lat, lng: pino1.long};
-
+		  var myLatLng = {lat: -8.18345560, lng: -34.92274761};
   		var options ={
-		 zoom: 8,
+		   zoom: 8,
     	 center: myLatLng,
    		 mapTypeControl: false
 		}
-		map = new google.maps.Map(document.getElementById('map'), options);
-		//AQUI CRIA O MARCADOR 
-		var marker = new google.maps.Marker({
-    	position: myLatLng,
-    	animation: google.maps.Animation.DROP,
-    	map: map,
-  		title: pino1.name
-
-   	 });
-		  marker.addListener('click', toggleBounce);
+  map = new google.maps.Map(document.getElementById('map'), options);
+  
+  if(locais1.length != -1){
+    addMarker(map, locais1);
+  }
 }
 
 
