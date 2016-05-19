51ca517da173bf23491b26ef877c2e4bafe748c4
Thierry <thierry.freire@hotmail.com>
2016-05-19 11:39:23 -0300

    Implementando métodos de buscar e remover usuário

--- Back-end/BackEnd/src/java/br/com/eclub/dao/UsuárioDAO.java
+++ Back-end/BackEnd/src/java/br/com/eclub/dao/UsuárioDAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
+
 /**
  *
  * @author Thierry Freire
@@ -22,11 +23,11 @@ public class UsuárioDAO {
     public Usuario BuscarUsuario (String RedeSocial) throws SQLException{
         Usuario aux = new Usuario();
         //Query no banco fazendo solicitação de busca da variável
-<<<<<<< HEAD
-         con = new ConnectionFactory().getConnection();
+
+         conLocal = new ConnectionFactory().getConnection();
          String s = "SELECT RedeSocial FROM Usuario WHERE RedeSocial = ?;";
          PreparedStatement stmt;
-         stmt = con.prepareStatement(s);
+         stmt = conLocal.prepareStatement(s);
          stmt.setString(1, aux.getRedeSocial());
          
          ResultSet rs = stmt.executeQuery();
@@ -41,40 +42,28 @@ public class UsuárioDAO {
          
          rs.close();
          stmt.close();
-         con.close();
+         conLocal.close();
          
          return aux; // falta implementar a busca
-=======
-         PreparedStatement stmt = conLocal.prepareStatement("SELECT FROM Usuario WHERE RedeSocial = ?");
-         stmt.setString(1, RedeSocial);
-         conLocal.close();
-         return null; // falta implementar a busca
->>>>>>> 621f06e054dca3b50ca3f0ddf3bca9d5b7f83500
+
+         
     }
     //Método de remover usuário
     public void RemoverUsuario (Usuario aux) throws SQLException{
         Usuario user = BuscarUsuario(aux.getRedeSocial());
         //Query de validar se o usuário exise para remoção;
-<<<<<<< HEAD
+
         if (user.getRedeSocial()!= null){
            String sql = ("DELETE FROM Usuario WHERE RedeSocial = ?;");
            
-         con = new ConnectionFactory().getConnection();
+         conLocal = new ConnectionFactory().getConnection();
          PreparedStatement stmt;
-         stmt = con.prepareStatement(sql);
+         stmt = conLocal.prepareStatement(sql);
          stmt.setString(1, aux.getRedeSocial());
          stmt.close();
-         con.close();
+         conLocal.close();
         }
       
-=======
-        if (usuario != null){
-            PreparedStatement stmt = conLocal.prepareStatement("DELETE FROM Usuario WHERE RedeSocial = ?");
-            stmt.setString(1, RedeSocial);
-            stmt.executeQuery();
-        }
-        conLocal.close();
->>>>>>> 621f06e054dca3b50ca3f0ddf3bca9d5b7f83500
     }
     //Método de Cadastrar usuário
     public Usuario CadastrarUsuario (Usuario usuario) throws SQLException{
