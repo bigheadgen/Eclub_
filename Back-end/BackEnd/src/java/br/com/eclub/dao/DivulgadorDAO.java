ab9316d41ae0bfc3b815d34e789c7d3ed2e25040
Thierry <thierry.freire@hotmail.com>
2016-05-24 13:50:46 -0300

    Altualizando estrutura da classe DivulgadorDAO

--- Back-end/BackEnd/src/java/br/com/eclub/dao/DivulgadorDAO.java
+++ Back-end/BackEnd/src/java/br/com/eclub/dao/DivulgadorDAO.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.eclub.dao;
 
 import br.com.eclub.modelo.Divulgador;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  *
@@ -20,19 +16,23 @@ public class DivulgadorDAO {
     
     
     }
-    public Divulgador BuscarDivulgador (String CNPJ){
+    //Método para buscar usuário divulgador
+    public Divulgador BuscarDivulgador (String CNPJ)throws SQLException{
         //Implemmentar método
         return null;
     }
-    public Divulgador CadastrarDivulgador (String CNPJ){
+    //Método para cadastrar usuário divulgador
+    public Divulgador CadastrarDivulgador (String CNPJ)throws SQLException{
         //Implementar método
         return null;
     }
-    public Divulgador AlterarDivulgador (String CNPJ){
+    //Método para alterar usuário divulgador
+    public Divulgador AlterarDivulgador (String CNPJ)throws SQLException{
         //Implementar método
         return null;
     }
-    public void RemoverDivulgador (String CNPJ){
+    //Método para remover usuário divulgador
+    public void RemoverDivulgador (String CNPJ)throws SQLException{
         //Implementar método
         
     }
