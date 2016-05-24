bd2ade559235cc098dc31fba45d74fb13b99b14b
Thierry-Freire <thierry.freire@hotmail.com>
2016-05-24 10:46:48 -0300

    Criando classe DivulgadorDAO e suas estruturas

---
+++ Back-end/BackEnd/src/java/br/com/eclub/dao/DivulgadorDAO.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.eclub.dao;
+
+import br.com.eclub.modelo.Divulgador;
+import java.sql.Connection;
+
+/**
+ *
+ * @author Thierry Freire
+ */
+public class DivulgadorDAO {
+    
+    private Connection conLocal;
+    //Construtor Defaut
+    public DivulgadorDAO() {
+    
+    
+    }
+    public Divulgador BuscarDivulgador (String CNPJ){
+        //Implemmentar método
+        return null;
+    }
+    public Divulgador CadastrarDivulgador (String CNPJ){
+        //Implementar método
+        return null;
+    }
+    public Divulgador AlterarDivulgador (String CNPJ){
+        //Implementar método
+        return null;
+    }
+    public void RemoverDivulgador (String CNPJ){
+        //Implementar método
+        
+    }
+}
