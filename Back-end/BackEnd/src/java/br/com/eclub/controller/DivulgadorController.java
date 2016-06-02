d5f8e90bef6e86c8885c60dc99548ac7f2db0cfd
Thierry-Freire <thierry.freire@hotmail.com>
2016-06-02 17:45:37 -0300

    Implementando métodos Remover e Alterar divulgador na camada Controller

--- Back-end/BackEnd/src/java/br/com/eclub/controller/DivulgadorController.java
+++ Back-end/BackEnd/src/java/br/com/eclub/controller/DivulgadorController.java
@@ -3,6 +3,7 @@ package br.com.eclub.controller;
 
 import br.com.eclub.dao.DivulgadorDAO;
 import br.com.eclub.modelo.Divulgador;
+import java.sql.SQLException;
 
 public class DivulgadorController {
     private DivulgadorDAO divulgadorDAO;
@@ -11,8 +12,26 @@ public class DivulgadorController {
     }
     public void CadastrarDivulgador (Divulgador divulgador){
         try{
-            if (divulgadorDAO.BuscarDivulgador(divulgador) != null){
+            if (divulgadorDAO.BuscarDivulgador(divulgador) == null){
                 divulgadorDAO.CadastrarDivulgador(divulgador);
+            }
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+    public void AlterarDivulgador (Divulgador divulgador) throws SQLException{
+        try {
+            if(divulgadorDAO.BuscarDivulgador(divulgador) != null){
+                divulgadorDAO.AlterarDivulgador(divulgador);
+            }
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+    public void RemoverDivulgador (Divulgador divulgador) throws SQLException{
+        try{
+            if (divulgadorDAO.BuscarDivulgador(divulgador) != null){
+                divulgadorDAO.RemoverDivulgador(divulgador);
             }
         } catch (Exception e){
             throw new RuntimeException(e);
