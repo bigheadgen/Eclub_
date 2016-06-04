4aa9f04f1940b47ea8633ec9d57b2dcafed23a8a
bigheadgen <gustavo_enndi@hotmail.com>
2016-06-04 06:33:24 -0300

    criando alguns eventos para adicionar ao mapa

--- Back-end/BackEnd/web/app/assets/javascript/mapa.js
+++ Back-end/BackEnd/web/app/assets/javascript/mapa.js
@@ -1,8 +1,70 @@
+
+
+// AQUI VAI SER A FUNÇÃO QUE CRIA E ADICIONA AO MAPA O MARCARDOR
+// FUNÇÃO QUE ADICONA N LOCAIS AO MAPA
+function queryString(parameter) {  
+              var loc = location.search.substring(1, location.search.length);   
+              var param_value = false;   
+              var params = loc.split("&");   
+              for (i=0; i<params.length;i++) {   
+                  param_name = params[i].substring(0,params[i].indexOf('='));   
+                  if (param_name == parameter) {                                          
+                      param_value = params[i].substring(params[i].indexOf('=')+1)   
+                  }   
+              }   
+              if (param_value) {   
+                  return param_value;   
+              }   
+              else {   
+                  return false;   
+              }   
+        }
+
+function  addEventos(eventos,city,map){
+  for (var i = 0; i < eventos.length; i++) {
+      if(eventos[i].cidade == city){
+          addMarker(eventos[i],map);
+      }
+  }
+}
+function addMarker(location, map) {
+  var myLatlng = new google.maps.LatLng(location.lat, location.long);
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    title: location.descricao
+});
+marker.setMap(map);
+}
+function getCidade(){
+ var variavel = queryString("local");
+  variavel = variavel.toLowerCase();
+  for (var i = 0; i < locais.length; i++) {
+    if(variavel == locais[i].name.toLowerCase()){
+      return locais[i];
+    }
+  }
+}
+function initMap() {
+  var cidade;
+  if(queryString("local") == false){
+    cidade = p1;
+  }else{
+    cidade = getCidade();
+  }
+    var mapOptions = {
+    zoom: cidade.zoom,
+    center: {lat: cidade.lat, lng:cidade.long},
+    disableDefaultUI: true
+  }
+  var map = new google.maps.Map(document.getElementById("map"),
+       mapOptions);
+  //falta finalizar 
+}
 var p1 = {
   name: "recife",
   lat: -8.0518067, 
   long:-34.9101193,
-  zoom: 13
+  zoom: 13,
 };
 var p2 = { 
   name: "olinda", 
@@ -52,50 +114,24 @@ var p8 = {
   long:-35.3067268,
   zoom: 14
 };
-
-var locais = [p1, p2, p3, p4, p5, p6, p7, p8];
-// AQUI VAI SER A FUNÇÃO QUE CRIA E ADICIONA AO MAPA O MARCARDOR
-// FUNÇÃO QUE ADICONA N LOCAIS AO MAPA
-function queryString(parameter) {  
-              var loc = location.search.substring(1, location.search.length);   
-              var param_value = false;   
-              var params = loc.split("&");   
-              for (i=0; i<params.length;i++) {   
-                  param_name = params[i].substring(0,params[i].indexOf('='));   
-                  if (param_name == parameter) {                                          
-                      param_value = params[i].substring(params[i].indexOf('=')+1)   
-                  }   
-              }   
-              if (param_value) {   
-                  return param_value;   
-              }   
-              else {   
-                  return false;   
-              }   
-        }
-
-function getCidade(){
- var variavel = queryString("local");
-  variavel = variavel.toLowerCase();
-  for (var i = 0; i < locais.length; i++) {
-    if(variavel == locais[i].name.toLowerCase()){
-      return locais[i];
-    }
-  }
-}
-function initMap() {
-  var cidade;
-  if(queryString("local") == false){
-    cidade = p1;
-  }else{
-    cidade = getCidade();
-  }
-    var mapOptions = {
-    zoom: cidade.zoom,
-    center: {lat: cidade.lat, lng:cidade.long},
-    disableDefaultUI: true
-  }
-  var map = new google.maps.Map(document.getElementById("map"),
-       mapOptions);
-
-}
+var locais = new Array(p1, p2, p3, p4, p5, p6, p7, p8);
+//eventos
+var ev1 ={
+  cidade:"recife",
+  descricao: "Nosso evento de teste1",
+  lat: -8.063696,
+  long: -34.8709444
+};
+var ev2 ={
+  cidade:"recife",
+  descricao: "Nosso evento de teste2",
+  lat: -8.0607854,
+  long: -34.8788971
+};
+var ev3 ={
+  cidade:"recife",
+  descricao: "Nosso evento de teste3",
+  lat: -8.0436238,
+  long: -34.8665268
+};
+var eventos = new Array(ev1, ev2, ev3);
