ed78e52b17800a5eb56c7b890dc38b1a3c0e7bd8
GugaE <Gustavo_enndi@hotmail.com>
2016-06-10 15:56:10 -0300

    atualização na pagina do divulgador

--- Back-end/workspace/EclubWS/WebContent/app/assets/javascript/page_divulgador.js
+++ Back-end/workspace/EclubWS/WebContent/app/assets/javascript/page_divulgador.js
@@ -1,7 +1,7 @@
-function Mudarestado(el) {
-    var display = document.getElementById(el).style.display;
-    if(display == "none")
-        document.getElementById(el).style.display = 'block';
-    else
-        document.getElementById(el).style.display = 'none';
+function mostra(id){
+	if(document.getElementById(id).style.display == 'block'){
+		document.getElementById(id).style.display = 'none';
+	}else{
+		document.getElementById(id).style.display = 'block';
+	}
 }
